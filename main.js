39746d5c3b7b133975befd34e8974a80b362d9f4
Pablo-Notebook <pcmuniz07@gmail.com>
2025-03-22 20:29:58 -0300

    added app icon on tray and created shotcut

---
+++ main.js
@@ -0,0 +1,59 @@
+const { app, BrowserWindow, Tray, Menu, globalShortcut } = require('electron');
+const path = require('path');
+
+let tray = null;
+let win = null;
+
+const createWindow = () => {
+  win = new BrowserWindow({
+    width: 400,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
+
+  win.on('closed', () => {
+    win = null;
+  });
+};
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, 'icon.png'));
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Mostrar App', click: () => { win.show(); } },
+    { label: 'Sair', click: () => { app.quit(); } }
+  ]);
+  tray.setToolTip('Meu App');
+  tray.setContextMenu(contextMenu);
+}
+
+function registerGlobalShortcut() {
+  globalShortcut.register('CommandOrControl+B', () => {
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+    }
+  });
+}
+
+app.on('ready', () => {
+  createWindow();
+  createTray();
+  registerGlobalShortcut();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
